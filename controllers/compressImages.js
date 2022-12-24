bf0d83854d4a9091eecd9523a0fe4c2cfaba5ff3
andriyanych <andriyanovsv@yandex.ru>
2022-12-24 17:58:09 +0800

    добавление модулей compress-images, chalk добавление рута compressImagesRouter и заготовки обработчика

--- controllers/compressImages.js
+++ controllers/compressImages.js
@@ -3,8 +3,8 @@ const path = require('path');
 const {baseUrlImageEstate, buildUrlImageEstate} = process.env;
 
 
-module.exports = compressImagesRouter(req, res, next) => {
+module.exports = compressImagesRouter = (req, res, next) => {
   //TODO: написать код, принимающий картинки с baseUrlImageEstate, после сжатия сохраняющий в buildUrlImageEstate и переписывающий
   console.log('req: ', req);
 
-}
+};
