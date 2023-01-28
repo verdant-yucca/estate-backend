1a15199e0d3f3db66b3a1fbad0d0405c86057449
Roman Davydenko <Deadshot323rd@yandex.ru>
2023-01-28 20:42:39 +0800

    small fix routes

--- routes/withAuth/routes.js
+++ routes/withAuth/routes.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
+const routerWithAuth = require('express').Router();
 const usersRouter = require('./users');
 const estateRouter = require('./estate');
 const contentRouter = require('./content');
 
 //TODO отправлять email в checkAuth
 
-router.use('/checkAuth', (req, res) => {res.send('admin@email.ru')})
-router.use(estateRouter);
-router.use(contentRouter);
-router.use(usersRouter);
+routerWithAuth.use('/checkAuth', (req, res) => {res.send('admin@email.ru')})
+routerWithAuth.use(estateRouter);
+routerWithAuth.use(contentRouter);
+routerWithAuth.use(usersRouter);
 
-module.exports = router;
+module.exports = routerWithAuth;
--- routes/withAuth/users.js
+++ routes/withAuth/users.js
@@ -1,12 +1,12 @@
 const usersRouter = require('express').Router();
 const {
   updateUserValidation,
-} = require('../middlewares/validatons');
+} = require('../../middlewares/validatons');
 
 const {
   getUsers,
   updateUser,
-} = require('../controllers/users');
+} = require('../../controllers/users');
 
 usersRouter.get('/users', getUsers);
 usersRouter.patch('/users/me', updateUserValidation, updateUser);
