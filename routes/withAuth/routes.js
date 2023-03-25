89efa29b0f481d3b8cdad5f38797ae2270483a9e
Roman Davydenko <deadshot323rd@yandex.ru>
2023-03-26 02:11:28 +0800

    small fix

--- routes/withAuth/routes.js
+++ routes/withAuth/routes.js
@@ -1,12 +1,14 @@
 const routerWithAuth = require('express').Router();
 const usersRouter = require('./users');
 const estateRouter = require('./estate');
+const reviewRouter = require('./review');
 const contentRouter = require('./content');
 
 //TODO отправлять email в checkAuth
 
 routerWithAuth.use('/checkAuth', (req, res) => {res.sendStatus(200);})
 routerWithAuth.use(estateRouter);
+routerWithAuth.use(reviewRouter);
 routerWithAuth.use(contentRouter);
 routerWithAuth.use(usersRouter);
 
